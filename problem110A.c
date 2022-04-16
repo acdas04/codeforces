1f0d36041006868b3762a7ada92cf4a5d16f4b57
acdas04 <101277709+acdas04@users.noreply.github.com>
2022-04-16 10:27:09 +0600

    Add files via upload

---
+++ problem110A.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    long long a,c;
+    
+    scanf("%lld",&a);
+    c=a;
+  int c
+
+  
+      while(n<count){
+          y=a%10;
+          n++;
+          x++;   
+          a=a-y;
+      
+      }
+    
+  return 0;
+}
